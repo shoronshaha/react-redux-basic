aabe71d6e912ec38892a8c3e1a0d2524c71b3a5c
Smaran Shaha <smaranshaha@gmail.com>
2024-12-28 21:34:22 +0600

    added middleware this is curried type working single single function type work

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,16 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const myLogger = (store) => (next) => (action) => {
+  console.log(`Action: ${JSON.stringify(action)}`);
+  console.log(`Before:${JSON.stringify(store.getState())}`);
+
+  const upcomingState = [action].reduce(rootReducer, store.getState());
+  console.log(JSON.stringify(upcomingState));
+
+  return next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
