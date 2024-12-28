c550a77244ff5c5f44696d35e848cc264bb64913
Smaran Shaha <smaranshaha@gmail.com>
2024-12-28 21:48:59 +0600

    added multiple middleware use by third party npm i redux-logger

---
+++ src/redux/middlewares/myLogger.js
@@ -0,0 +1,13 @@
+import rootReducer from "../rootReducer";
+
+const myLogger = (store) => (next) => (action) => {
+  console.log(`Action: ${JSON.stringify(action)}`);
+  console.log(`Before:${JSON.stringify(store.getState())}`);
+
+  const upcomingState = [action].reduce(rootReducer, store.getState());
+  console.log(JSON.stringify(upcomingState));
+
+  return next(action);
+};
+
+export default myLogger;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
+import myLogger from "./middlewares/myLogger";
+import { logger } from "redux-logger";
 
-const myLogger = (store) => (next) => (action) => {
-  console.log(`Action: ${JSON.stringify(action)}`);
-  console.log(`Before:${JSON.stringify(store.getState())}`);
-
-  const upcomingState = [action].reduce(rootReducer, store.getState());
-  console.log(JSON.stringify(upcomingState));
-
-  return next(action);
-};
-
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(rootReducer, applyMiddleware(logger, myLogger));
 
 export default store;
